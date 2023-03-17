5ba6a03a9670a91170073d5bee155f15e69969c1
unknown <SENA@MABAPRRATFSD043.FormacionSena.RED>
2023-03-17 14:13:59 -0500

    primero comentario compai

---
+++ app/Controllers/ContactoController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Controllers;
+
+class contactocontroller extends BaseController
+{
+    Public function index(){
+        //eturn view('contacto/inicio'); 
+         $data=['titulo' => 'inicio'];
+      $vista=view ('contacto/Header', $data).
+             view ('contacto/Menu').
+             view ('contacto/inicio').
+             view ('contacto/Footer');
+    return $vista;
+            
+            }
+    Public function catalogo($numeroCatalogo){
+
+        $data= ['titulo' => 'catalogo'];
+        $catalogo=['numero' => $numeroCatalogo];
+        echo view ('contacto/Header', $data);
+        echo view ('contacto/Menu');
+        echo view ('contacto/catalogo'), $catalogo;
+        echo view ('contacto/Footer');
+    }
+
+
+}
+
+?>
---
+++ app/Views/contacto/catalogo.php
@@ -0,0 +1,29 @@
+<div class="card" style="width: 18rem;">
+
+<?php
+if ($numero==0){
+?>
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">CATALOGO 1</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<?php
+} else if ($numero==1){
+?>
+
+<img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">CATALOGO 1</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+
+<?php
+}
+?>
+
+</div>
